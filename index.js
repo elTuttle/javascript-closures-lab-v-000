4d3e624f0f6788527d79860e893da84b3feb8320
John Tuttle <thejwt@gmail.com>
2017-12-07 03:39:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 const app = "I don't do much."
+
+function bumpCounter() {
+  var counter = 0;
+
+  function addBump(){
+    counter++;
+  }
+
+  function getBumps(){
+    return counter;
+  }
+
+  return {
+    addBump,
+    getBumps
+  };
+
+}
+
+function createAnimal(animalType){
+  function createDevice(deadlyDevice){
+    return {
+      animalType,
+      deadlyAnimal
+    };
+
+  }
+
+  return createDevice;
+}
+
+const sharkCreator = createAnimal('Shark');
+
+var sharkWithFrickinLaserbeam = sharkCreator('Laserbeam');
+var sharkWithFrickinCannon = createDevice('Cannon')
